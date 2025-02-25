62866d5b0508012e494452f95d0d4e00c8aae534
baghayun <jhhu1027@gmail.com>
2025-02-26 00:56:51 +0900

    bbi

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const API_KEY=`9b1e78b6560b498cb57862f612b14d18`
 let newsList = [];
 const getLatesNews = async() => {
   const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
+  // new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
   //자바스크립트는 개발자가 필요로 하는 기본함수 제공해줌 -> 그중 하나가 URL 
   const response = await fetch(url);
   const data = await response.json();
